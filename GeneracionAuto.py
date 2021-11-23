4bc5af571990686a0cd85d7283b72741dc47dd5b
Daniel Araya Sambucci <danielarayasambucci@gmail.com>
2021-11-22 22:28:36 -0600

    Laberinto HARD agregado, implementación de las funciones
    El programa ya está trabajando en su totalidad. Queda refinar el fitness para que funcione correctamente y ayude al progreso de las generaciones

--- GeneracionAuto.py
+++ GeneracionAuto.py
@@ -1,6 +1,8 @@
 from Individuo import *
-from random import randint
+import random
 from pruebasColores import pintarIndividuos
+import numpy as np
+from PIL import Image
 
 def validarPosicion(listaIndividuos, nuevoIndividuo):
     for individuo in listaIndividuos:
@@ -8,15 +10,35 @@ def validarPosicion(listaIndividuos, nuevoIndividuo):
             return False
     return True
 
-def PrimeraGeneracion(cantIndividuos):
+def PrimeraGeneracion(cantIndividuos, laberinto):
     if (cantIndividuos > 2500):
         return print("Cantidad de individuos inválida")
     listaIndividuos = []
     while cantIndividuos != 0:
-        x = randint(0,49)
-        y = randint(0,49)
+        x = int(random.uniform(0,49))
+        y = int(random.uniform(0,49))
         individuo = Individuo(x, y, 0)
-        individuo.setPuntaje(randint(1,1000))
+        
+        if (laberinto == 'laberinto-easy'):
+            img = np.array(Image.open('laberinto-easy.png'))
+        elif (laberinto == 'laberinto-medium'):
+            img = np.array(Image.open('laberinto-medium.png'))
+        elif (laberinto == 'laberinto-hard'):
+            img = np.array(Image.open('laberinto-hard.png'))
+
+        if img[y,x][0] == 0 and img[y,x][1] == 255 and img[y,x][2] == 0: # verde
+            individuo.setColor(2)
+            individuo.setPuntaje(1000)
+        elif img[y,x][0] == 0 and img[y,x][1] == 0 and img[y,x][2] == 255: # azul
+            individuo.setColor(3)
+            individuo.setPuntaje(1000)
+        elif img[y,x][0] == 255 and img[y,x][1] == 255 and img[y,x][2] == 255:
+            individuo.setColor(0)
+            individuo.setPuntaje(random.randint(50,200))
+            #individuo.setPuntaje(1000)
+        else:
+            individuo.setColor(1)
+            individuo.setPuntaje(0)
 
         if not (validarPosicion(listaIndividuos, individuo)):
             continue
@@ -27,8 +49,8 @@ def PrimeraGeneracion(cantIndividuos):
     for i in listaIndividuos:
         print("Posicion x: " + str(i.getX()) + ", Posicion Y: " + str(i.getY()))
 
-    pintarIndividuos(listaIndividuos)
-    return
+    pintarIndividuos(listaIndividuos, laberinto)
+    return listaIndividuos
 
-#PrimeraGeneracion(70)
+#PrimeraGeneracion(70, 'laberinto-easy')
 
